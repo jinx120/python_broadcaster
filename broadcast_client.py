ea5ae8a4f492b2eeeb94bb3b908d26d968a9f146
jinx120 <64343389+jinx120@users.noreply.github.com>
2021-09-26 11:00:15 -0700

    Update broadcast_client.py

--- broadcast_client.py
+++ broadcast_client.py
@@ -4,8 +4,16 @@ import os
 
 host = input("Enter host address: ")
 port = input("Enter port: ") 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+try:
+    port = int(port)
+except:
+    print("Failed to convert port string to integer!")
+
+try: 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+except:
+    print("An error occured connecting! Make sure server is running and the informating you entered is correct!")
 
 def recieve():
     while True:
